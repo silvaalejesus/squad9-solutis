7c24c55ee2903b66d66ba38a92485a5994734437
ricardovac <ricardoviicius@gmail.com>
2023-08-05 19:38:46 -0300

    feat: adicionando exercícios 2, 7, 10 e 12

---
+++ Lista_Exercicios_01/src/Exercicio07.java
@@ -0,0 +1,27 @@
+public class Exercicio07 {
+    public static void main(String[] args) {
+        // Exercício 07
+        System.out.println("7. Imprima o resultado da divisão por 2 de todos os múltiplos de 3, entre 1 e 100, usando os tipos de dados int e double.");
+
+        // Utilizando integer
+        System.out.println("Utilizando integer:");
+        for (int i = 1; i <= 100; i++) {
+            // Apenas múltiplos de 3
+            if (i % 3 == 0) {
+                int result = i / 2;
+                System.out.printf("%d ", result);
+            }
+        }
+
+        System.out.println();
+
+        // Utilizando double
+        System.out.println("Utilizando double:");
+        for (double i = 1; i <= 100; i++) {
+            if (i % 3 == 0) {
+                double result = i / 2;
+                System.out.printf("%.2f ", result);
+            }
+        }
+    }
+}
