ec69d9b5c5d880d3f201315d2c8fefec20ca16e8
silvaalejesus <silvaalejesus@gmail.com>
2023-08-08 01:32:34 -0300

    feat: adicionando as questoes 02, 07, 27, 34, 37, 49, 65

---
+++ src/lista_exercicios_03/Fibonacci_49.java
@@ -0,0 +1,16 @@
+package lista_exercicios_03;
+
+public class Fibonacci_49 {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 0;
+        int temp;
+
+        for (int i = 0; i < 50; i++) {
+            temp = a;
+            a = a + b;
+            b = temp;
+            System.out.println(b);
+        }
+    }
+}
