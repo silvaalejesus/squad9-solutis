cd6f8c3bc25d1caef436999277cc5cf23ce6a189
Victor Kayk <victorkayk77@gmail.com>
2023-08-09 02:35:48 -0300

    Lista 5 - Questão 3 - Empregado

---
+++ src/lista_exercicios_05/TesteEmpregado.java
@@ -0,0 +1,14 @@
+package lista_exercicios_05;
+
+public class TesteEmpregado {
+    public static void main(String[] args) {
+        Empregado empregado = new Empregado("Paulo", "(77) 981562351", "Rua Paulo", 21, 15000, 17.2);
+        System.out.println("Nome: " + empregado.getNome());
+        System.out.println("Telefone: " + empregado.getTelefone());
+        System.out.println("Endereço: " + empregado.getEndereco());
+        System.out.println("Código do setor: " + empregado.getCodigoSetor());
+        System.out.println("Salário base: R$"+ empregado.getSalarioBase());
+        System.out.println("Imposto: " + empregado.getImposto());
+        System.out.println("Salário: " + empregado.calcularSalario());
+    }
+}
