bc7bf890e353f7691d0526f0efddd168f26c2961
ricardovac <ricardoviicius@gmail.com>
2023-08-09 14:17:42 -0300

    feat: exercicio 5 funcionarios

---
+++ src/lista_exercicios_06/FuncionarioEnsinoBasico.java
@@ -0,0 +1,18 @@
+package lista_exercicios_06;
+
+public class FuncionarioEnsinoBasico extends Funcionario {
+    private String escola;
+
+    public FuncionarioEnsinoBasico(String nome, int codigoFuncional, String escola) {
+        super(nome, codigoFuncional);
+        this.escola = escola;
+    }
+
+    public String getEscola() {
+        return escola;
+    }
+
+    public void setEscola(String escola) {
+        this.escola = escola;
+    }
+}
---
+++ src/lista_exercicios_06/FuncionarioSemEstudo.java
@@ -0,0 +1,7 @@
+package lista_exercicios_06;
+
+public class FuncionarioSemEstudo extends Funcionario {
+    public FuncionarioSemEstudo(String nome, int codigoFuncional) {
+        super(nome, codigoFuncional);
+    }
+}
