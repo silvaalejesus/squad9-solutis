bee1733aaa3a134fd90d4e0d448e021cfe3432ef
Victor Kayk <victorkayk77@gmail.com>
2023-08-09 03:00:03 -0300

    Lista 6 - Questão 1 - Animais

---
+++ src/lista_exercicios_06/Preguica.java
@@ -0,0 +1,17 @@
+package lista_exercicios_06;
+
+public class Preguica extends Animal {
+	public Preguica(String nome, int idade) {
+		super(nome, idade);
+	}
+
+	@Override
+	public void emitirSom() {
+		System.out.println("A preguiça " + this.getNome() + " faz um som: Ah");
+	}
+
+	@Override
+	public void acao() {
+		System.out.println("A preguiça " + this.getNome() + " vai subir em árvores");
+	}
+}
